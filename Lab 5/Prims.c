ab3adb8490527017b4703b19e1a164549f7e7060
MahediBuilds <zainmahedi.cs23@bmsce.ac.in>
2025-04-07 09:54:22 +0530

    Refactor Prim's algorithm implementation to eliminate parent tracking and simplify MST construction

--- Lab 5/Prims.c
+++ Lab 5/Prims.c
@@ -1,67 +1,50 @@
 #include <stdio.h>
 #include <limits.h>
-#include <stdbool.h>
 
-#define V 5 // Number of vertices in the graph
+#define V 5
 
-// Returns the vertex with the minimum key value not yet in MST
-int minKey(int key[], bool mstSet[]) {
-    int min = INT_MAX, minIndex;
-    for (int v = 0; v < V; v++) {
-        if (!mstSet[v] && key[v] < min) {
-            min = key[v];
-            minIndex = v;
-        }
-    }
-    return minIndex;
-}
+void primMSTNoParent(int graph[V][V]) {
+    int visited[V] = {0};
+    int fromEdge[V - 1], toEdge[V - 1], edgeWeight[V - 1];
+    int edgeCount = 0;
+    visited[0] = 1;
 
-// Prints the constructed MST
-void printMST(int parent[], int graph[V][V]) {
-    printf("Edge \tWeight\n");
-    for (int i = 1; i < V; i++) {
-        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]);
-    }
-}
-
-// Constructs and prints MST using Prim's algorithm
-void primMST(int graph[V][V]) {
-    int parent[V];
-    int key[V];
-    bool mstSet[V];
-
-    for (int i = 0; i < V; i++) {
-        key[i] = INT_MAX;
-        mstSet[i] = false;
-    }
-
-    key[0] = 0;
-    parent[0] = -1;
-
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, mstSet);
-        mstSet[u] = true;
-
-        for (int v = 0; v < V; v++) {
-            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = graph[u][v];
+    for(int i = 0; i < V - 1; i++) {
+        int minWeight = INT_MAX;
+        int fromVertex = -1;
+        int toVertex = -1;
+        for(int u = 0; u < V; u++) {
+            if(visited[u] == 1) {
+                for(int v = 0; v < V; v++) {
+                    if(visited[v] == 0 && graph[u][v] != 0 && graph[u][v] < minWeight) {
+                        minWeight = graph[u][v];
+                        fromVertex = u;
+                        toVertex = v;
+                    }
+                }
             }
         }
+        fromEdge[edgeCount] = fromVertex;
+        toEdge[edgeCount] = toVertex;
+        edgeWeight[edgeCount] = minWeight;
+        edgeCount++;
+        visited[toVertex] = 1;
     }
 
-    printMST(parent, graph);
+    printf("Edge\tWeight\n");
+    for(int i = 0; i < edgeCount; i++) {
+        printf("%d - %d\t%d\n", fromEdge[i], toEdge[i], edgeWeight[i]);
+    }
 }
 
 int main() {
-    int graph[V][V] = {
-        {0, 2, 0, 6, 0},
-        {2, 0, 3, 8, 5},
-        {0, 3, 0, 0, 7},
-        {6, 8, 0, 0, 9},
-        {0, 5, 7, 9, 0}
-    };
+    int graph[V][V];
+    for(int i = 0; i < V; i++) {
+        for(int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
 
-    primMST(graph);
+    primMSTNoParent(graph);
     return 0;
 }
